54fc6a77abb59da0003b30deef261050e66eefb6
D0405 <kupczyk097@yahoo.de>
2023-06-20 14:20:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     #app.run(debug=True) #TODO Remove later
-    app.run(host="202.61.200.166", port=5000) #TODO Remove later
+    app.run(host="202.61.200.166", port=80) #TODO Remove later
     
